407abae993cb9aaf7fb166d6bda695132fc8b036
Ben Long <blong14@lemur.localdomain>
2022-05-30 09:52:01 -0400

    Adding tracing to gctl

--- cmd/gctl/main.go
+++ cmd/gctl/main.go
@@ -4,27 +4,70 @@ import (
 	"context"
 	"encoding/csv"
 	"fmt"
-	grepl "github.com/blong14/gache/internal/actors/replication"
-	gproxy "github.com/blong14/gache/internal/proxy"
-	gwal "github.com/blong14/gache/internal/wal"
 	"log"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
 
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/jaeger"
+	"go.opentelemetry.io/otel/sdk/resource"
+	tracesdk "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.10.0"
+
 	gactors "github.com/blong14/gache/internal/actors"
+	grepl "github.com/blong14/gache/internal/actors/replication"
+	gerrors "github.com/blong14/gache/internal/errors"
 	grpc "github.com/blong14/gache/internal/io/rpc"
+	gproxy "github.com/blong14/gache/internal/proxy"
+	gwal "github.com/blong14/gache/internal/wal"
 )
 
 var done chan struct{}
+
+const (
+	service     = "gctl"
+	environment = "production"
+	id          = 1
+)
+
+func tracerProvider(url string) (*tracesdk.TracerProvider, error) {
+	// Create the Jaeger exporter
+	exp, err := jaeger.New(jaeger.WithCollectorEndpoint(jaeger.WithEndpoint(url)))
+	if err != nil {
+		return nil, err
+	}
+	tp := tracesdk.NewTracerProvider(
+		// Always be sure to batch in production.
+		tracesdk.WithBatcher(exp),
+		// Record information about this application in a Resource.
+		tracesdk.WithResource(resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceNameKey.String(service),
+			attribute.String("environment", environment),
+			attribute.Int64("ID", id),
+		)),
+	)
+	return tp, nil
+}
 
 func main() {
 	if err := os.Setenv("DEBUG", "false"); err != nil {
 		log.Fatal(err)
 	}
+	if err := os.Setenv("TRACE", "false"); err != nil {
+		log.Fatal(err)
+	}
 	sigint := make(chan os.Signal, 1)
 	signal.Notify(sigint, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	tp, err := tracerProvider("http://jaeger.cluster/api/traces")
+	if err != nil {
+		log.Fatal(err)
+	}
+	otel.SetTracerProvider(tp)
 
 	ctx, cancel := context.WithCancel(context.Background())
 
@@ -56,6 +99,10 @@ func main() {
 	}
 	log.Printf("received %s signal\n", s)
 	gproxy.StopProxy(ctx, qp)
+	errs := gerrors.Append(tp.ForceFlush(ctx), tp.Shutdown(ctx))
+	if errs.ErrorOrNil() != nil {
+		log.Println(errs)
+	}
 	cancel()
 	time.Sleep(500 * time.Millisecond)
 }
